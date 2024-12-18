fedfb637c5fb5dd2d698fb5295eec468d04bfed0
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2024-12-18 19:24:54 +0530

    Dashboard ui completed

---
+++ client/src/components/ChatContainer.jsx
@@ -0,0 +1,9 @@
+const ChatContainer=()=>{
+    return(
+        <div>
+            This is chat container
+        </div>
+    )
+};
+
+export default ChatContainer;
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,48 @@
+import TinderCard from "react-tinder-card";
+import { useState } from "react";
+import ChatContainer from "../components/ChatContainer";
 const Dashboard=()=>{
+    const characters = [
+        {
+          name: 'Richard Hendricks',
+          url: 'https://i.imgur.com/oPj4A8u.jpeg'
+        },
+        {
+          name: 'Erlich Bachman',
+          url: 'https://i.imgur.com/Q9WPlWA.jpeg'
+        },
+        {
+          name: 'Monica Hall',
+          url: 'https://i.imgur.com/MWAcQRM.jpeg'
+        }
+      ]
+    const [lastDirection, setLastDirection] = useState()
+  
+    const swiped = (direction, nameToDelete) => {
+      console.log('removing: ' + nameToDelete)
+      setLastDirection(direction)
+    }
+  
+    const outOfFrame = (name) => {
+      console.log(name + ' left the screen!')
+    }
     return(
-        <div>
-            <h1>This is Dashboard page!</h1>
+        <div className="dashboard">
+            <ChatContainer/>
+            <div className="swipe-container">
+                <div className="card-container">
+                {characters.map((character) =>
+                <TinderCard className='swipe' key={character.name} onSwipe={(dir) => swiped(dir, character.name)} onCardLeftScreen={() => outOfFrame(character.name)}>
+                    <div style={{ backgroundImage: 'url(' + character.url + ')' }} className='card'>
+                        <h3>{character.name}</h3>
+                    </div>
+                </TinderCard>)}
+                <div className="swipe-info">
+                    {lastDirection?<p>You swiped {lastDirection}</p>:<p></p>}
+                </div>
+                </div>
+            </div>
+            {/* <h1>This is Dashboard page!</h1> */}
         </div>    
     )
 }
