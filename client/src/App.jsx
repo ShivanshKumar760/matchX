01f3acbe97e5ce59c863d0e89c339f8d99eccb17
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2024-12-19 14:26:17 +0530

    App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import Home from './pages/Home'
 import Dashboard from './pages/Dashboard'
 import OnBoarding from './pages/Onboarding'
-import {BrowserRouter,Routes,Route,Navigate} from "react-router-dom";
+import {BrowserRouter,Routes,Route} from "react-router-dom";
 import { useCookies } from 'react-cookie';
 function App() {
   const [count, setCount] = useState(0)
@@ -10,22 +10,14 @@ function App() {
   const authToken=cookies.AuthToken;
   return (
     <>
-     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        
-        {/* Protected routes */}
-        {authToken ? (
-          <>
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/onboarding" element={<OnBoarding />} />
-          </>
-        ) : (
-          // Redirect to home or login if not authenticated
-          <Route path="/dashboard" element={<Navigate to="/" />} />
-        )}
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+            
+            <Route path="/" element={<Home/>}/>
+            <Route path="/dashboard" element={<Dashboard/>}/>
+            <Route path="/onboarding" element={<OnBoarding/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
