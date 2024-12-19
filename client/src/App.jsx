deba81810219aded3f74095e56dfb3fb5cc748e5
Shivansh Kumar <104713531+ShivanshKumar760@users.noreply.github.com>
2024-12-19 14:16:22 +0530

    Navigate added -> app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import Home from './pages/Home'
 import Dashboard from './pages/Dashboard'
 import OnBoarding from './pages/Onboarding'
-import {BrowserRouter,Routes,Route} from "react-router-dom";
+import {BrowserRouter,Routes,Route,Navigate} from "react-router-dom";
 import { useCookies } from 'react-cookie';
 function App() {
   const [count, setCount] = useState(0)
